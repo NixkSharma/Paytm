188f3b70635bdca98ad16e94ae199d046f498902
NixkSharma <nixksharma@gmail.com>
2024-11-29 01:48:39 +0530

    added route to get user balance

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,4 +1,14 @@
 const express = require('express');
+const authMiddleware = require('../middleware');
+const { Account } = require('../db');
 const router = express.Router();
+
+router.get('/balance', authMiddleware, async(req, res)=>{
+    const user = await Account.findOne({userId : req.userId});
+    console.log(user.balance);
+    res.json({
+        balance : user.balance
+    });
+});
 
 module.exports = router;
