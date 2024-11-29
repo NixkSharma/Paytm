7f4f2fefdc8352b635f8649d891163b8f033688a
NixkSharma <nixksharma@gmail.com>
2024-11-29 23:41:07 +0530

    added routes in app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import { Signup } from './pages/signup'
+import { Dashboard } from './pages/dashboard'
+import { Send } from './pages/send'
+import { Signin } from './pages/signin'
 
 function App() {
   return (
-    <>
-    </>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/signup' element= {<Signup />} />
+          <Route path='/signin' element= {<Signin />} />
+          <Route path='/dashboard' element= {<Dashboard />} />
+          <Route path='/send' element= {<Send />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
