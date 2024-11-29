1bc731ecdcd899ac9f46a5102f41da467f8832a4
NixkSharma <nixksharma@gmail.com>
2024-11-30 00:40:58 +0530

    created SendMoney component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import { Signup } from './pages/signup'
 import { Dashboard } from './pages/dashboard'
-import { Send } from './pages/send'
+import { SendMoney } from './pages/SendMoney'
 import { Signin } from './pages/signin'
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
           <Route path='/signup' element= {<Signup />} />
           <Route path='/signin' element= {<Signin />} />
           <Route path='/dashboard' element= {<Dashboard />} />
-          <Route path='/send' element= {<Send />} />
+          <Route path='/send' element= {<SendMoney />} />
         </Routes>
       </BrowserRouter>
     </div>
