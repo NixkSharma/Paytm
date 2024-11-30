633b0ae2a9630931657e0f5b492b20a6ca4fecd2
NixkSharma <nixksharma@gmail.com>
2024-12-01 00:44:38 +0530

    connected frontend to backend

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,12 +1,24 @@
+import { useState, useEffect } from "react"
 import { Appbar } from "../components/Appbar"
 import { Balance } from "../components/Balance"
 import { Users } from "../components/Users"
+import axios from "axios"
 
 export const Dashboard = () => {
+    const [balance, setBalance] = useState(0);
+    useEffect(()=>{
+       axios.get('http://localhost:3000/api/v1/account/balance',{
+            headers : {
+                Authorization : `Bearer ${localStorage.getItem('token')}`
+            }
+       }).then((res) => {
+            setBalance(res.data.balance);
+       }); 
+    },[])
     return <div>
         <Appbar />
         <div className="m-8">
-            <Balance value={"10,000"} />
+            <Balance value={balance} />
             <Users />
         </div>
     </div>
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,3 +1,6 @@
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 import { BottomWarning } from "../components/BottomWarning"
 import { Button } from "../components/Button"
 import { Heading } from "../components/Heading"
@@ -5,17 +8,31 @@ import { InputBox } from "../components/InputBox"
 import { SubHeading } from "../components/SubHeading"
 
 export const Signup = () => {
+    const navigate = useNavigate()
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [userName, setuserName] = useState("");
+    const [password, setPassword] = useState("");
     return <div className="bg-slate-300 h-screen flex justify-center">
     <div className="flex flex-col justify-center">
       <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
         <Heading label={"Sign up"} />
         <SubHeading label={"Enter your infromation to create an account"} />
-        <InputBox placeholder="John" label={"First Name"} />
-        <InputBox placeholder="Doe" label={"Last Name"} />
-        <InputBox placeholder="harkirat@gmail.com" label={"Email"} />
-        <InputBox placeholder="123456" label={"Password"} />
+        <InputBox onchange={e => setFirstName(e.target.value)} placeholder="John" label={"First Name"} />
+        <InputBox onchange={e => setLastName(e.target.value)}  placeholder="Doe" label={"Last Name"} />
+        <InputBox onchange={e => setuserName(e.target.value)} placeholder="harkirat@gmail.com" label={"Email"} />
+        <InputBox onchange={e => setPassword(e.target.value)} placeholder="123456" label={"Password"} />
         <div className="pt-4">
-          <Button label={"Sign up"} />
+          <Button onClick={async()=>{
+            const response = await axios.post('http://localhost:3000/api/v1/user/signup', {
+              firstName,
+              lastName,
+              userName, 
+              password
+            });
+            navigate('/dashboard');
+            localStorage.setItem('token', response.data.token);
+          }} label={"Sign up"} />
         </div>
         <BottomWarning label={"Already have an account?"} buttonText={"Sign in"} to={"/signin"} />
       </div>
